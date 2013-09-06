b34802a472632dd715952c8d993d80008a73bcbb
Chad A. Carino <chadc@tekserve.com>
2013-09-06 18:25:08 -0400

    Add static content layout, add fixed nav.

--- js/ui-elements.js
+++ js/ui-elements.js
@@ -3,27 +3,21 @@ function drawerSlide(drawerID, buttonID){
 $j('#'+drawerID).slideToggle();
 $j('#'+buttonID).toggleClass("opened");
 }
-var $j = jQuery;
-function drawerSlide(drawerID, buttonID){
-$j('#'+drawerID).slideToggle();
-$j('#'+buttonID).toggleClass("opened");
-}
 var wrapperbgcolor;
-$j('.section').each(function() {
+$j(".section").each(function() {
 	wrapperbgcolor = $j(this).css('background-color');
-	switch (wrapperbgcolor) {
-		case "#f36f37":
-			wrapperbgcolor = 'orange';
-			break;
-		case "#004d72":
-			wrapperbgcolor = 'darkblue';
-			break;
-		case "#40a8c9":
-			wrapperbgcolor = 'lightblue';
-			break;
-		default:
-			wrapperbgcolor = '';
-	}
 	$j(this).wrap('<div class="wrap" />');
-	$j(this > '.wrap').wrap('<div class="bgwrapper '+wrapperbgcolor+'" />');
+	$j(this > '.wrap').wrap('<div class="bgwrapper" style="background="'+wrapperbgcolor+'" />');
 });
+function fixDiv() {
+    var $div = $j("#nav");
+    if ($j(window).scrollTop() > $div.data("top")) { 
+        $j('#nav').css({'position': 'fixed', 'top': '0', 'width': '100%'}); 
+    }
+    else {
+        $j('#nav').css({'position': 'static', 'top': 'auto', 'width': '100%'});
+    }
+}
+
+$j("#nav").data("top", $j("#nav").offset().top); // set original position on load
+$j(window).scroll(fixDiv);
---
+++ static_content.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file adds the Static Content page template to the Apparition Child Theme.
+ *
+ * @author Chad A. Carino
+ * @package Apparition - Tekserve
+ * @subpackage Customizations
+ */
+
+/*
+Template Name: Static Content
+*/
+
+// Add custom body class to the head
+
+
+// Remove header, navigation, breadcrumbs, footer widgets, footer 
+
+remove_action( 'genesis_before_header', 'genesis_do_nav' );
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs');
+remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
+remove_action( 'genesis_post_title', 'genesis_do_post_title' );
+
+genesis();
