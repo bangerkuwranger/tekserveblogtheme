0fa5cf8079da988880ea642211ebb96f6e0aa4b5
Chad A. Carino <chadc@tekserve.com>
2014-01-30 16:38:14 -0500

    added iNSIDER to icaps. Added MTA styles for train lines. Added larger maxheights for long form content. Fixed pp spacing issue. Allowed media mention to be in a class.

--- js/ui-elements.js
+++ js/ui-elements.js
@@ -269,6 +269,19 @@ $j('document').ready(function() { //call on load
 			$j(this).css('height', opptxtheight);
 		}
 	});
+	
+	//icaps
+	$j('h1, h2, h3, .detailBoxTrigger, .drawertrigger').each(function () {
+		var my_html = $j(this).html();
+		var my_old_html = $j(this).html();
+ 
+		// Make sure there are no children so we don't edit more than we want.
+		if ($j(this).children().length == 0) {
+			my_html = my_html.replace(/(iPad|iPhone|iMac|iPod|iOS|Mac OS X|MacBook|Mac mini|Mac Pro)/ig, '<span class="trademark">$1</span>');
+			my_html = my_html.replace(/(iNSIDER)/ig, '<span class="insider">$1</span>');
+			$j(this).html(my_html);
+		}
+	});
 
 	$j('.section').removeClass('vc_row-fluid');  //removes vc_fluid badness from sections
 
@@ -341,15 +354,4 @@ $j('document').ready(function() { //call on load
 		}
 	}, 1000);
 	
-});
-//icaps
-$j('h1, h2, h3, .detailBoxTrigger, .drawertrigger').each(function () {
-    var my_html = $j(this).html();
-    var my_old_html = $j(this).html();
- 
-    // Make sure there are no children so we don't edit more than we want.
-    if ($j(this).children().length == 0) {
-        my_html = my_html.replace(/(iPad|iPhone|iMac|iPod|iOS|Mac OS X|MacBook|Mac mini|Mac Pro)/ig, '<span class="trademark">$1</span>');
-        $j(this).html(my_html);
-    }
 });
