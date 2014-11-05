b1f68aaf8ae9d9c70ba91284fee295d51ec80d72
Chad A. Carino <chadc@tekserve.com>
2014-11-05 17:01:31 -0500

    Updates to iCaps

--- js/icaps.js
+++ js/icaps.js
@@ -14,7 +14,8 @@ $j('h1, h2, h3, h1 a, h2 a, h3 a, .detailBoxTrigger, .drawertrigger, .tekserve-c
 
 	// Make sure there are no children so we don't edit more than we want.
 	if ($j(this).children().length == 0) {
-		my_html = my_html.replace(/(iPad|iPhone|iMac|iPod|iOS|Mac OS X|Mac mini|Mac Pro|MacBook Pro|MacBook Air|iCloud|iLife|iWork|iPhoto|iMovie|iCal|iTunes|AppleCare|Apple)/ig, '<span class="trademark">$1</span>');
+		my_html = my_html.replace(/(AirDrop|AirMac|AirPlay|AirPort Express|AirPort Extreme|AirPort Time Capsule|AirTunes|Aperture|Apple|Apple Cinema Display|AppleScript|AppleScript Studio|AppleShare|AppleTalk|Apple TV|AppleWorks|App Nap|Aqua|Back to My Mac|Bonjour|Boot Camp|Claris|ColorSync|Cover Flow|Dashcode|DVD Studio Pro|Exposé|FaceTime|FairPlay|FileVault|Final Cut Pro|Finder|FireWire|GarageBand|iBook|iBooks|iCal|iChat|iDVD|iLife|iMac|iMessage|iMovie|Inkwell|iPad|iPhone|iPhoto|iPod classic|iPod nano|iPod shuffle|iPod Socks|iPod touch|iSight|iTunes|iWork|Keychain|Keynote|LiveType|MacApp|MacBook Air|MacBook Pro|MacDNS|Macintosh|Mac OS|Mac Pro|MacTCP|MagSafe|MainStage|Mission Control|Objective-C|OpenCL|OpenPlay|OS X|Passbook|Photo Booth|Pixlet|PowerBook|Power Mac|Quartz|QuickDraw|QuickTime|Retina|Rosetta|Safari|Sherlock|Siri|Smart Cover|SuperDrive|Time Capsule|Time Machine|TrueType|WaveBurner|WebObjects|Xcode|Xgrid|Xsan|Xserve)/ig, '<span class="rtrademark">$1</span>');
+		my_html = my_html.replace(/(AirPrint|Apple CarPlay|AppleLink|Apple Pay|Apple Remote Desktop|Apple Studio Display|AppleVision|Apple Watch|Cinema Tools|DVD@CCESS|EarPods|eMac|EtherTalk|Flyover|iBeacon|ImageWriter|iPad Air|iPad mini|iWeb|LaserWriter|Lightning|LocalTalk|Multi-Touch|NetInfo|Newton|OfflineRT|Photocasting|ProDOS|SnapBack|StyleWriter|TestFlight|Touch ID)/ig, '<span class="trademark">$1</span>');
 		my_html = my_html.replace(/(iNSIDER)/ig, '<span class="insider">$1</span>');
 		$j(this).html(my_html);
 	}
