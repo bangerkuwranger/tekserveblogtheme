37b1a42b9048fdad3f811507ceaa3da750718f0c
Chad A. Carino <chadc@tekserve.com>
2015-03-25 14:38:50 -0400

    TOC code replaced in main js, updates to sidebar styles

--- js/apparition.js
+++ js/apparition.js
@@ -5,6 +5,7 @@
 var $j = jQuery;
 var clientWidth = document.documentElement.clientWidth;
 var $rev_slider;
+var tocLink = '<a href="#toc_container" class="toclink" title="Go to table of contents"><i class="fa fa-caret-square-o-up"></i> TABLE OF CONTENTS</a>';
 
 function detailBoxBinder() {
 
@@ -287,6 +288,9 @@ $j(window).bind('load', function() {
 	initTekNavSubmenu();
 	
 	initTitleOnAjaxFormSubmit();
+	
+	//set toc links
+	initFaqToc();
 	
 	//run fixdiv once to insure correct behavior on load
 	fixDiv();
@@ -740,3 +744,44 @@ function isElementVisible(el) {
     );
     
 }	//end isElementVisible(el)
+
+/******
+	move images in headings to toc and add toc class to body
+******/
+
+function initFaqToc() {
+
+	if($j('#toc_container').length > 0) {
+		
+		$j('body').addClass('has-toc');
+		$j('#toc_container>.toc_list>li').each(function() {
+
+			var theAnchor = $j(this).children('a').attr('href');
+			console.log(theAnchor);
+			var theTarget = $j(theAnchor);
+			if (theTarget.children('img').length > 0) {
+
+				var theImage = theTarget.children('img').attr('src');
+				theImage = 'url(' + theImage + ')';
+				$j(this).css('backgroundImage', theImage);
+
+			} //end if (theTarget.children('img').length > 0)
+
+		}); //end $j('#toc_container>.toc_list>li').each(function()
+		
+		//move first paragraph to position under title (wraps around featured image)
+		var $firstP = $j('div.entry-content>p:first-child').detach();
+		$j('.entry-title').after($firstP);
+		$j('.entry-title').after('<hr style="margin-bottom:2em;" />');
+		
+		//add TOC links before every body H2 except first
+		$j('.entry-content h2').before(tocLink);
+		$j('.entry-content a.toclink:first-of-type').remove();
+		
+		//bind toc anchors
+		goToAnchor();
+		bindAnchors();
+
+	} //end if($j('#toc_container').length > 0)
+	
+}	//end initFaqToc()
