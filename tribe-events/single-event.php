a1d6ae2997665ba7ffb61465579de0a70d814daf
Chad A. Carino <chadc@tekserve.com>
2015-05-14 17:48:07 -0400

    added broad classes for text alignment adjustment; they're very soft. tweaked single event layout and styles to be less... what they were

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -57,50 +57,42 @@ if( !empty( $event_link ) ) {
 
 <div id="tribe-events-content" class="tribe-events-single">
 
-	<p class="tribe-events-back"><a href="<?php echo tribe_get_events_link() ?>"> <?php _e( '&laquo; All Events', 'tribe-events-calendar' ) ?></a></p>
+	<p class="right"><a class="tribe-events-back button" style="color:#fff;" href="<?php echo tribe_get_events_link() ?>"> <?php _e( '&laquo; All Events', 'tribe-events-calendar' ) ?></a></p>
 
 	<!-- Notices -->
 	<?php tribe_events_the_notices() ?>
-
-	<?php the_title( '<h2 class="tribe-events-single-event-title summary">', '</h2>' ); ?>
-
-	<div class="tribe-events-schedule updated published tribe-clearfix">
-		<h3><?php echo $datedetails ?></h3>
-		<!-- Event meta -->
-		<?php do_action( 'tribe_events_single_event_before_the_meta' ) ?>
-		<div class="tribe-events-single-event-venue"
-			<h3>At <?php echo tribe_get_venue_link() ?> - <?php echo tribe_get_full_address(); ?>
-		<?php  if( tribe_get_cost() ) :  ?>
-			<span class="tribe-events-divider">|</span>
-			<span class="tribe-events-cost"><?php echo tribe_get_cost( null, true ) ?></span>
-		<?php endif; ?>
-			</h3>
-		</div>
-		<?php echo $organizer; ?>
-		<?php echo $event_link ?>
-		
-		<?php echo tribe_events_event_recurring_info_tooltip(); ?>
-		
-	</div>
-	<?php do_action( 'tribe_events_single_event_after_the_meta' ) ?>
 	<!-- Event header -->
 	<div id="tribe-events-header" <?php tribe_events_the_header_attributes() ?>>
-		<!-- Navigation -->
-		<h3 class="tribe-events-visuallyhidden"><?php _e( 'Event Navigation', 'tribe-events-calendar' ) ?></h3>
-		<ul class="tribe-events-sub-nav">
-			<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '&laquo; %title%' ) ?></li>
-			<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% &raquo;' ) ?></li>
-		</ul><!-- .tribe-events-sub-nav -->
 	</div><!-- #tribe-events-header -->
 
 	<?php while ( have_posts() ) :  the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class('vevent'); ?>>
 			<!-- Event featured image -->
 			<?php echo tribe_event_featured_image(); ?>
+			<?php the_title( '<h2 class="tribe-events-single-event-title summary">', '</h2>' ); ?>
+			<div class="tribe-events-schedule updated published tribe-clearfix">
+				<h2>About This Event</h2>
+				<h3><?php echo $datedetails ?></h3>
+				<!-- Event meta -->
+				<?php do_action( 'tribe_events_single_event_before_the_meta' ) ?>
+				<div class="tribe-events-single-event-venue"
+					<h3>At <?php echo tribe_get_venue_link() ?> - <?php echo tribe_get_full_address(); ?>
+				<?php  if( tribe_get_cost() ) :  ?>
+					<span class="tribe-events-divider">|</span>
+					<span class="tribe-events-cost"><?php echo tribe_get_cost( null, true ) ?></span>
+				<?php endif; ?>
+					</h3>
+				</div>
+				<?php echo $organizer; ?>
+				<?php echo $event_link ?>
+		
+				<?php echo tribe_events_event_recurring_info_tooltip(); ?>
+		
+			</div>
+			<?php do_action( 'tribe_events_single_event_after_the_meta' ) ?>
 			<!-- Event content -->
 			<?php do_action( 'tribe_events_single_event_before_the_content' ) ?>
 			<div class="tribe-events-single-event-description tribe-events-content entry-content description">
-				<h2>About This Event</h2>
 				<?php the_content(); ?>
 			</div>
 			<!-- .tribe-events-single-event-description -->
